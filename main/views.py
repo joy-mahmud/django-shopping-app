24ce593d8ded4b3a3c6d910f48c80105a1254426
joy-mahmud <joymahmudiucse@gmail.com>
2024-12-19 23:05:38 +0600

    add product and show product

---
+++ main/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from .forms import AddProduct
+from .models import Product
+# Create your views here.
+def home(request):
+    products=Product.objects.all()
+    context={
+        'products':products
+    }
+    
+    return render(request,'main/home.html',context)
+
+
+def add_product(request):
+    if request.method=="POST":
+        form=AddProduct(request.POST)
+        if form.is_valid():
+            form.save()
+            
+            return redirect("home")
+    
+    form=AddProduct()
+    context={
+        'form':form
+    }
+    return render(request,'main/addProduct.html',context)
