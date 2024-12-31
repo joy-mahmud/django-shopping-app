148c96f20da9fb44b2da8a949f1dc82dcca19a9e
joy-mahmud <joymahmudiucse@gmail.com>
2024-12-31 23:28:48 +0600

    add to cart and view cart completed

--- main/urls.py
+++ main/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('',views.home, name="home"),
     path('addProduct/',views.add_product, name="add_product"),
     path('productDetails/<int:id>', views.product_details, name='product_details'),
-    path('addToCart/',views.add_to_cart, name="add_to_cart")
+    path('addToCart/',views.add_to_cart, name="add_to_cart"),
+    path('cart/',views.view_cart, name="cart"),
 ]
--- main/views.py
+++ main/views.py
@@ -4,6 +4,7 @@ from .forms import AddProduct
 from .models import Product,Cart,CartItem
 from django.views.decorators.csrf import csrf_exempt
 import json
+from django.db.models import Sum,F
 
 # Create your views here.
 def home(request):
@@ -46,14 +47,26 @@ def add_to_cart(request):
         product_id=body.get('product_id')
         product = Product.objects.get(id=product_id)
         cart,created = Cart.objects.get_or_create(user=request.user)
+        print(created)
         
         cart_item,item_created= CartItem.objects.get_or_create(cart=cart,product=product)
+        print('item created',item_created)
         if not item_created :
             cart_item.quantity+=1
             cart_item.save()
             
-        return HttpResponse('product added to cart')
+        return JsonResponse({'redirect_url':'http://127.0.0.1:8000/cart/'})
     # cart=Cart(product_id=id)
     # cart.save()
+    
+def view_cart(request):
+    cart,created=Cart.objects.get_or_create(user=request.user)
+    items=cart.items.select_related('product').annotate(
+        item_total=F('quantity') * F('product__price')
+    )
+    
+    total_price = items.aggregate(total=Sum('item_total'))['total'] or 0
+
+    return render(request,'main/view_cart.html',{'items':items,'total_price':total_price})
     
     
